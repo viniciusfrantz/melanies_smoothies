75180f6b759e2163210c1f5e480a43c19b8bfbc9
Vinicius Giacomini Frantz <85130783+viniciusfrantz@users.noreply.github.com>
2025-04-11 17:00:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,9 @@ st.write('The name in your order will be:', name_on_order)
 #     '''INSERT INTO smoothies.public.order(name_on_order)
 # values'''(customer)''' ''')
 
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
-
                                                                       
 pd_df = my_dataframe.to_pandas
 st.dataframe(pd_df)
