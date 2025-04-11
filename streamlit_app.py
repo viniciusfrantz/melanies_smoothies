c75c5305d9a8b6d3fce90e8f9f58307aa3469583
Vinicius Giacomini Frantz <85130783+viniciusfrantz@users.noreply.github.com>
2025-04-11 16:59:34 -0300

    Update streamlit_app.py
    corrected my_dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,10 @@ st.write('The name in your order will be:', name_on_order)
 # values'''(customer)''' ''')
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), (col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
-st.write('Teste Vinciius2')
                                                                       
 pd_df = my_dataframe.to_pandas
 st.dataframe(pd_df)
