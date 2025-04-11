e5883838f05a3706f9e07bc6445c0a942c269808
Vinicius Giacomini Frantz <85130783+viniciusfrantz@users.noreply.github.com>
2025-04-11 16:54:36 -0300

    Update streamlit_app.py
    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
+st.write('Teste Vinciius')
+                                                                      
 pd_df = my_dataframe.to_pandas
 st.dataframe(pd_df)
 st.stop()
