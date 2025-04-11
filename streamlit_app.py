0e7fc98434889ec81a5740704f52377d78e62ae1
Vinicius Giacomini Frantz <85130783+viniciusfrantz@users.noreply.github.com>
2025-04-11 10:42:10 -0300

    Update streamlit_app.py
    Corrected API site request issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list: #same that if ingredient_list is not null
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ' '
         st.subheader(ingredient + 'Nutrition_Information')
-        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/" + ingredient)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredient)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     st.write(ingredients_string)
