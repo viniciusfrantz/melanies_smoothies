046dcf5dc5277f649e4a506f2d447d8a0c44f79c
Vinicius Giacomini Frantz <85130783+viniciusfrantz@users.noreply.github.com>
2025-04-11 17:09:51 -0300

    Update streamlit_app.py
    added search by column search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
                                                                       
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -40,6 +40,10 @@ if ingredients_list: #same that if ingredient_list is not null
     
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ' '
+
+        search_on = pd_df.loc[pd_df.loc[pd_df['FRUIT_NAME'] == ingredient, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', ingredient, ' is ', search_on, '.')
+      
         st.subheader(ingredient + 'Nutrition_Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ingredient)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
